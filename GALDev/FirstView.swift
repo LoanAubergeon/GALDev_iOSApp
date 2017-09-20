a3395df8ea2b06276f50d8905604b4751e93b547
LoanAubergeon <laubergeon@gmail.com>
2017-09-20 11:07:02 +0200

    Simple changes

--- GALDev/FirstView.swift
+++ GALDev/FirstView.swift
@@ -9,117 +9,119 @@
 import UIKit
 import GoogleMaps
 
+
+/// The first view of the app when you are authenticated
 class FirstView: UIViewController, CLLocationManagerDelegate {
-    
-    
+
+
     // Variables
-    
+
     var token = Home.GlobalsVariables.userToken
 
     @IBOutlet weak var menuButton: UIBarButtonItem!
-    
+
     @IBOutlet var viewMap : GMSMapView?
-    
+
     @IBOutlet var lblInfo : UILabel?
-    
+
     let locationManager = CLLocationManager()
-    
+
     var didFindMyLocation = false
-    
+
     var mapTasks = MapTasks()
-    
+
     var locationMarker: GMSMarker!
-    
-    
-    
+
+
+
     var originMarker: GMSMarker!
-    
+
     var destinationMarker: GMSMarker!
-    
+
     var routePolyline: GMSPolyline!
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
     // ***********************************************************************************************************************
-    
-    
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        
+
         if self.revealViewController() != nil {
             menuButton.target = self.revealViewController()
             menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
             self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         }
-        
+
         //print("Voici le token :")
         //print(token)
-        
-        
+
+
         let camera: GMSCameraPosition = GMSCameraPosition.camera(withLatitude: 48.857165, longitude: 2.354613, zoom: 8.0)
         viewMap?.camera = camera
         viewMap?.settings.myLocationButton = true
-        
+
         locationManager.delegate = self
         locationManager.requestWhenInUseAuthorization()
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.startUpdatingLocation()
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
+
     @IBAction func changeMapType(sender: AnyObject) {
-        
+
         // For change the map type with little menu
-        
+
         let actionSheet = UIAlertController(title: "Map Types", message: "Select map type :", preferredStyle: UIAlertControllerStyle.actionSheet)
-        
+
         let normalMapTypeAction = UIAlertAction(title: "Normal", style: UIAlertActionStyle.default) { (alertAction) -> Void in
             self.viewMap?.mapType = kGMSTypeNormal
         }
-        
+
         let terrainMapTypeAction = UIAlertAction(title: "Terrain", style: UIAlertActionStyle.default) { (alertAction) -> Void in
             self.viewMap?.mapType = kGMSTypeTerrain
         }
-        
+
         let hybridMapTypeAction = UIAlertAction(title: "Hybrid", style: UIAlertActionStyle.default) { (alertAction) -> Void in
             self.viewMap?.mapType = kGMSTypeHybrid
         }
-        
+
         let cancelAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
-            
+
         }
-        
+
         actionSheet.addAction(normalMapTypeAction)
         actionSheet.addAction(terrainMapTypeAction)
         actionSheet.addAction(hybridMapTypeAction)
         actionSheet.addAction(cancelAction)
-        
+
         present(actionSheet, animated: true, completion: nil)
     }
-    
-    
+
+
     @IBAction func findAddress(sender: AnyObject) {
         let addressAlert = UIAlertController(title: "Address Finder", message: "Type the address you want to find:", preferredStyle: UIAlertControllerStyle.alert)
-        
+
         addressAlert.addTextField { (textField) -> Void in
             textField.placeholder = "Address?"
         }
-        
+
         let findAction = UIAlertAction(title: "Find Address", style: UIAlertActionStyle.default) { (alertAction) -> Void in
             let address = (addressAlert.textFields![0] as UITextField).text!
-            
+
             self.mapTasks.geocodeAddress(address: address, withCompletionHandler: { (status, success) -> Void in
                 if !success {
                     print(status)
-                    
+
                     if status == "ZERO_RESULTS" {
                         self.showAlertWithMessage(message: "The location could not be found.")
                     }
@@ -131,38 +133,38 @@ class FirstView: UIViewController, CLLocationManagerDelegate {
                     self.setuplocationMarker(coordinate: coordinate)
                 }
             })
-            
+
         }
-        
+
         let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
-            
+
         }
-        
+
         addressAlert.addAction(findAction)
         addressAlert.addAction(closeAction)
-        
+
         present(addressAlert, animated: true, completion: nil)
     }
-    
-    
-    
+
+
+
     @IBAction func createRoute(sender: AnyObject) {
         let addressAlert = UIAlertController(title: "Create Route", message: "Connect locations with a route:", preferredStyle: UIAlertControllerStyle.alert)
-        
+
         addressAlert.addTextField { (textField) -> Void in
             textField.placeholder = "Origin?"
         }
-        
+
         addressAlert.addTextField { (textField) -> Void in
             textField.placeholder = "Destination?"
         }
-        
-        
+
+
         let createRouteAction = UIAlertAction(title: "Create Route", style: UIAlertActionStyle.default) { (alertAction) -> Void in
-            
+
             let origin = (addressAlert.textFields![0] ).text!
             let destination = (addressAlert.textFields![1] ).text!
-            
+
             self.mapTasks.getDirections(origin: origin, destination: destination, waypoints: nil, travelMode: nil, completionHandler: { (status, success) -> Void in
                 if success {
                     self.viewMap?.clear()
@@ -175,20 +177,20 @@ class FirstView: UIViewController, CLLocationManagerDelegate {
                 }
             })
         }
-        
+
         let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
-            
+
         }
-        
+
         addressAlert.addAction(createRouteAction)
         addressAlert.addAction(closeAction)
-        
+
         present(addressAlert, animated: true, completion: nil)
     }
-    
-    
+
+
     // ***********************************************************************************************************************
-    
+
     // Verify the location authorization
     @nonobjc func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
         if status == CLAuthorizationStatus.authorizedWhenInUse {
@@ -197,7 +199,7 @@ class FirstView: UIViewController, CLLocationManagerDelegate {
             viewMap?.settings.myLocationButton = true
         }
     }
-    
+
     // For indicate the current location
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let userLocation = locations.last
@@ -205,25 +207,25 @@ class FirstView: UIViewController, CLLocationManagerDelegate {
                                               longitude: userLocation!.coordinate.longitude, zoom: 13.0)
         viewMap?.isMyLocationEnabled = true
         viewMap?.camera = camera
-        
+
         locationManager.stopUpdatingLocation()
     }
-    
-    
+
+
     // Fonction pour affficher une fenetre d'alerte
     func showAlertWithMessage(message: String) {
         let alertController = UIAlertController(title: "GetALift", message: message, preferredStyle: UIAlertControllerStyle.alert)
-        
+
         let closeAction = UIAlertAction(title: "Close", style: UIAlertActionStyle.cancel) { (alertAction) -> Void in
-            
+
         }
-        
+
         alertController.addAction(closeAction)
-        
+
         present(alertController, animated: true, completion: nil)
     }
-    
-    
+
+
     // Fonction pour ajouter a marker au lieu rechercher
     func setuplocationMarker(coordinate: CLLocationCoordinate2D) {
         locationMarker = GMSMarker(position: coordinate)
@@ -233,38 +235,38 @@ class FirstView: UIViewController, CLLocationManagerDelegate {
         //locationMarker.icon = GMSMarker.markerImage(with: UIColor.blue)
         locationMarker.opacity = 0.75
     }
-    
-    
+
+
     func configureMapAndMarkersForRoute() {
         viewMap?.camera = GMSCameraPosition.camera(withTarget: mapTasks.originCoordinate, zoom: 15.0)
-        
+
         originMarker = GMSMarker(position: self.mapTasks.originCoordinate)
         originMarker.map = self.viewMap
         originMarker.icon = GMSMarker.markerImage(with: UIColor.green)
         originMarker.title = self.mapTasks.originAddress
-        
+
         destinationMarker = GMSMarker(position: self.mapTasks.destinationCoordinate)
         destinationMarker.map = self.viewMap
         destinationMarker.icon = GMSMarker.markerImage(with: UIColor.red)
         destinationMarker.title = self.mapTasks.destinationAddress
     }
-    
+
     func drawRoute() {
         let route = mapTasks.overviewPolyline["points"] as! String
-        
+
         let path: GMSPath = GMSPath(fromEncodedPath: route)!
         routePolyline = GMSPolyline(path: path)
         routePolyline.strokeWidth = 5
         routePolyline.strokeColor = UIColor.blue
         routePolyline.map = viewMap
     }
-    
+
     func displayRouteInfo() {
         DispatchQueue.main.async() {
             self.lblInfo?.text = self.mapTasks.totalDistance + " " + self.mapTasks.totalDuration
         }
     }
-    
-    
-    
+
+
+
 }
