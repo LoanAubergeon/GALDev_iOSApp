cd36a75aee1ce3795df493c18dc1d9ae196d355b
Loan <laubergeon@gmail.com>
2017-11-01 13:28:47 +0100

    ~ Fix

--- GALDev/GetMoreInformation.swift
+++ GALDev/GetMoreInformation.swift
@@ -6,10 +6,11 @@
 //  Copyright © 2017 Loan Aubergeon. All rights reserved.
 //
 
-import Foundation
-import UIKit
 
-class GetMoreInformation: UIViewController {
+import UIKit
+import MessageUI
+
+class GetMoreInformation: UIViewController, MFMailComposeViewControllerDelegate {
     
     @IBOutlet weak var menuButton:UIBarButtonItem!
     
@@ -28,6 +29,40 @@ class GetMoreInformation: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func openWeb (sender: Any){
+        guard let url = URL(string: "http://laubergeon.fr") else {
+            return
+        }
+        
+        if #available(iOS 10.0, *) {
+            UIApplication.shared.open(url, options: [:], completionHandler: nil)
+        } else {
+            UIApplication.shared.openURL(url)
+        }
+    }
+    
+    
+    @IBAction func mailCompose (sender: Any){
+        //Envoie d'un mail
+        //On crée un composeur de mails
+        let mc = MFMailComposeViewController()
+        //On lui donne son "délégué"
+        mc.mailComposeDelegate = self
+        //On donne des destinataires au mail
+        mc.setToRecipients(["laubergeon@gmail.com"])
+        //On donne un sujet
+        mc.setSubject("Informations about GetALift")
+        //Et on peut même écrire le corps du texte
+        mc.setMessageBody("...", isHTML: false)
+        //On le montre
+        self.present(mc, animated: true, completion: nil)
+        
+    }
+    
+    // Appelée lorsque l'utilisateur a fini avec ses mails
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
     
     
 }
